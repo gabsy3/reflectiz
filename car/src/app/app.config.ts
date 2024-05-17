eec8b8ad3b676c7673f6d2ce9d79ab485c33c94e
gabsy3 <gabsy3@gmail.com>
2024-05-17 18:02:47 +0300

    message with toastr

--- car/src/app/app.config.ts
+++ car/src/app/app.config.ts
@@ -3,10 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), BrowserAnimationsModule,provideToastr()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), BrowserAnimationsModule,provideToastr(),provideAnimations()]
 };
