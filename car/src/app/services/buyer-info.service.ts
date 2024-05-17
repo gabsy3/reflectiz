c2652fe49d5a61eb95bbc5fdd0148ee2e7e8b042
gabsy3 <gabsy3@gmail.com>
2024-05-17 23:27:34 +0300

    add getLocalStorage service

--- car/src/app/services/buyer-info.service.ts
+++ car/src/app/services/buyer-info.service.ts
@@ -5,11 +5,15 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
   providedIn: 'root'
 })
 export class BuyerInfoService {
-
+  arr: any = [];
   constructor() { }
 
   sendRequset(formDetails: any) {
-    window.localStorage.setItem("buyer-info", JSON.stringify(formDetails.value));
+    this.arr.push(formDetails.value)
+    window.localStorage.setItem("buyer-info", JSON.stringify(this.arr));
+  }
+  getDataFromLocalStorage() {
+    return window.localStorage.getItem("buyer-info");
   }
   checkDateNotGratherThenToday(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
