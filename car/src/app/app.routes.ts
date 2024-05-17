97327e4b2af3972988c763cfc73b8db3ed76736b
gabsy3 <gabsy3@gmail.com>
2024-05-17 09:53:58 +0300

    create component

--- car/src/app/app.routes.ts
+++ car/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
+    {path:'',component:LandingPageComponent},
+    {path:'landing-page',component:LandingPageComponent},
+    {path:'dashboard',component:DashboardComponent},
 ];
