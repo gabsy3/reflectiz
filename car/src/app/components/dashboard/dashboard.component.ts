07e5da3ce1bd6c2129ee64dcee49d031408cff7a
gabsy3 <gabsy3@gmail.com>
2024-05-18 10:30:33 +0300

    dashboard

--- car/src/app/components/dashboard/dashboard.component.ts
+++ car/src/app/components/dashboard/dashboard.component.ts
@@ -15,23 +15,42 @@ export class DashboardComponent implements OnInit {
   dataSource: any;
   displayedColumns: string[] = ['favoriteColor', 'birthDate'];
 
-  motorTypeByGenderDataSource:any;
+  motorTypeByGenderDataSource: any;
   motorTypeByGenderColumn: string[] = ['motorType', 'gender'];
 
-  mostVisitorsDataSource:any;
+  mostVisitorsDataSource: any;
   mostVisitorsColumn: string[] = ['city'];
-  
+  cityWithMostVisitors: string[] = [];
+  mostVisitorsCount: number = 0;
+
   constructor(private buyerInfoService: BuyerInfoService) { }
   ngOnInit(): void {
-    this.dataSource =  this.motorTypeByGenderDataSource = this.mostVisitorsDataSource =  this.buyerInfoService.getDataFromLocalStorage();
-    this.dataSource.sort((a: { birthDate: string; }, b: { birthDate: string; }) => (a.birthDate > b.birthDate) ? 1 : ((b.birthDate > a.birthDate) ? -1 : 0))
-    this.motorTypeByGenderDataSource.sort((a: { gender: string; }, b: { gender: string; }) => (a.gender > b.gender) ? 1 : ((b.gender > a.gender) ? -1 : 0))
-    // this.mostPickedColorByAge();
+    this.dataSource = this.motorTypeByGenderDataSource = this.mostVisitorsDataSource = this.buyerInfoService.getDataFromLocalStorage();
+    // this.dataSource.sort((a: { birthDate: string; }, b: { birthDate: string; }) => (a.birthDate > b.birthDate) ? 1 : ((b.birthDate > a.birthDate) ? -1 : 0))
+    // this.motorTypeByGenderDataSource.sort((a: { gender: string; }, b: { gender: string; }) => (a.gender > b.gender) ? 1 : ((b.gender > a.gender) ? -1 : 0))
+    
+    this.findMostVisitedCity();
+   
   }
-  // mostPickedColorByAge(){
-  // let colorsArr = this.dataSource.map((item:any) =>item.favoriteColor);
-  // let colors = colorsArr.filter((value: any, index: any) => colorsArr.indexOf(value) === index);
-  //   this.dataSource.sort((a: { birthDate: string; },b: { birthDate: string; }) => (a.birthDate > b.birthDate) ? 1 : ((b.birthDate > a.birthDate) ? -1 : 0))
-  // }
+  findMostVisitedCity() {
+    const cityCounts = new Map<string, number>();
 
+    this.mostVisitorsDataSource.forEach((visitor:any) => {
+      const visitorCity = visitor.location.city;
+      const currentCount = cityCounts.get(visitorCity) || 0;
+      cityCounts.set(visitorCity, currentCount + 1);
+    });
+
+    let mostVisitorsCity = '';
+    let mostVisitors = 0;
+    cityCounts.forEach((count, city) => {
+      if (count > mostVisitors) {
+        mostVisitorsCity = city;
+        mostVisitors = count;
+      }
+    });
+    
+    this.cityWithMostVisitors.push(mostVisitorsCity);
+    this.mostVisitorsCount = mostVisitors;
+  }
 }
